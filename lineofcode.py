6cf1e1e1d2645593c103d206ca4b5fb7e9f71eb2
enriqueperaltajimenez <enriqueperalta1@icloud.com>
2025-01-25 13:08:20 +0100

    this is my script for the homework for sessions 1 and 2.

---
+++ lineofcode.py
@@ -0,0 +1,15 @@
+print("this is the first homework task I do for this class.", end="")
+print("During this task, I'll tell you a bit about myself")
+print("I'm Enrique, I am Spanish but I have mostly attended school in Switzerland")
+print("my hobbies are:" "football", "skiing", "listening to music", " spending time with the people I care about", "hiking", sep=", ", end=".\n" )
+#in python, we can use hashtag to make comments on our code.
+#\n is used to star a new line after "end"
+#during our first session we also learnt about print also being a calculator for example:
+print("what is 5 to the power of two?", 5**2)
+print("during this task we are also required to add a readme file which I will do now.")
+print("this will now be the last line of code for this homework",end=". ")
+print("Thanks for reading.")
+
+
+
+
